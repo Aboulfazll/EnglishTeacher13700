93edf38045f0edd82fb7c259998d62faba444723
Aboulfazll <hasaanpour49@gmail.com>
2026-09-18 00:43:44 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui:1.7.8")
-    implementation("androidx.foundation:foundation:1.7.8")
+    implementation("androidx.compose.foundation:foundation:1.7.8")
     implementation("androidx.compose.material3:material3:1.3.1")
 }
