81fe9c2204ff42ac81518abbf5213948a275609f
Aboulfazll <hasaanpour49@gmail.com>
2026-09-18 00:35:03 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,12 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    plugins {
+        id("com.android.application") version "8.7.3"
+        id("org.jetbrains.kotlin.android") version "2.0.21"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
+    }
 }
 
 dependencyResolutionManagement {
