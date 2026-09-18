6469652d0c4d2d726cb9d584c55548005464a1f3
Aboulfazll <hasaanpour49@gmail.com>
2026-09-19 00:42:32 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,9 +31,16 @@ android {
 }
 
 dependencies {
+    // Compose & Core
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.foundation:foundation:1.7.8")
     implementation("androidx.compose.material3:material3:1.3.1")
+
+    // Navigation بین صفحات
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+
+    // آیکون‌های اضافه
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 }
