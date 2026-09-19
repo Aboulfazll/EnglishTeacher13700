a7b61e0636397fa88ede8f13f4350afd8231027b
Aboulfazll <hasaanpour49@gmail.com>
2026-09-19 19:45:32 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,19 +31,16 @@ android {
 }
 
 dependencies {
-    // Compose & Core
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.foundation:foundation:1.7.8")
     implementation("androidx.compose.material3:material3:1.3.1")
-
-    // Navigation
-    implementation("androidx.navigation:navigation-compose:2.8.5")
-
-    // Icons
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-
-    // Coil - لود عکس از اینترنت
+    implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
