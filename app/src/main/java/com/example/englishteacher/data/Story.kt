acbd62d30bda0f2330f86f557afb9ea7391d9544
Aboulfazll <hasaanpour49@gmail.com>
2026-09-21 16:55:47 +0330

    Update Story.kt

--- app/src/main/java/com/example/englishteacher/data/Story.kt
+++ app/src/main/java/com/example/englishteacher/data/Story.kt
@@ -1,10 +1,10 @@
 package com.example.englishteacher.data
 
 data class Story(
-    val id: Int,
+    val id: String, // تغییر از Int به String
     val title: String,
     val titlePersian: String,
-    val level: String, // اگر در کدهایتان level را عددی (Int) تعریف کرده‌اید، این را به Int تغییر دهید
+    val level: Level, // تغییر از String به Level
     val coverUrl: String,
     val text: String,
     val moral: String,
